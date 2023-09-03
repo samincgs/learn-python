76b46c9290c920f710a5724ac9ff4335084fde54
samin <samincgs@gmail.com>
2023-09-03 13:41:13 -0400

    learned the difference between classes and methods

---
+++ 07-python-classes/more_classes_and_methods.py
@@ -0,0 +1,47 @@
+# a function executes a block of code
+# a method belongs to an object and executes a block of code
+
+
+# def add(a, b):
+#     return a + b
+
+
+# class Test:
+#     def __init__(self, add_function):
+#         self.add_function = add_function
+
+
+# test = Test(add_function=add)
+# print(test.add_function(2, 4))
+
+# create a Monster class with a parameter called func, store this func as parameter
+# create another class Attacks: with bite,strike,slash,kick (each method just prints some text)
+
+
+class Monster:
+    def __init__(self, func):
+        self.func = func
+
+
+class Attacks:
+    def __init__(self):
+        pass
+
+    def bite(self):
+        print("BITES")
+
+    def strike(self):
+        print("STRIKE")
+
+    def slash(self):
+        print("SLASH")
+
+    def kick(self):
+        print("KICK")
+
+
+attacks = Attacks()
+monster = Monster(attacks.bite)
+print(dir(monster))
+
+monster.func()
