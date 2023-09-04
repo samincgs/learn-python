5392fcc0820584c8a7ff15a300fb96b84afb122a
samin <samincgs@gmail.com>
2023-09-04 13:07:36 -0400

    learned about creating my own modules and exporting/importing them onto different files

---
+++ 08-python-modules/custom_modules.py
@@ -0,0 +1,11 @@
+# import import_for_module as test_file
+import import_for_module as test_file
+
+# test_file.test_func("Hi")
+
+# print(test_file.test_var)
+
+# player = test_file.Player(health=100, dmg=20, energy=15)
+
+sum_total = test_file.sum_calc(2,5,10,12,15)
+print(sum_total)
---
+++ 08-python-modules/import_for_module.py
@@ -0,0 +1,17 @@
+# def test_func(content):
+#     print(f"This is an imported function with the parameter: {content}")
+
+# test_var = "test"
+
+# class Player:
+#     def __init__(self, health, dmg, energy):
+#         self.health = health
+#         self.dmg = dmg
+#         self.energy = energy
+
+# # create a sum calculator function 
+# # that takes unlimited argumesnt and returns the sum
+
+def sum_calc(*args):
+    return sum(args)
+
