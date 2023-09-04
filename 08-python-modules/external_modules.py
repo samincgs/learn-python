55a4837f38e2f8e2a98a0c17a221770bec85da71
samin <samincgs@gmail.com>
2023-09-04 03:47:35 -0400

    learned about external modules made by other programmers. Some examples of the modules used are pyautogui and matplotlib

---
+++ 08-python-modules/external_modules.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+
+# import pyautogui
+# from time import sleep
+
+# sleep(1)
+# pyautogui.write("This is written by a computer", interval=0.25)
+
+# exercise
+# create a graph from matplotlib
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.ylabel("some numbers")
+plt.xlabel("time")
+plt.show()
