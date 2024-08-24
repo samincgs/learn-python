dfc2a58d0c481d54efaa831e99dc5928ad35e86f
samin <samincgs@gmail.com>
2024-08-24 14:31:56 -0400

    learned a lot more about functions and deepdived into list comprehensions

--- python_basics/functions_7.py
+++ python_basics/functions_7.py
@@ -2,8 +2,11 @@
 
 # def keyword stands for definition
 # we use functions to reuse code without repeating ourselves
+# we use functions to take an input value and do calculations and return an output
 
 def hello_func():
-    print('hello function')
+    return 'Hello Function'
 
-hello_func()
+print(hello_func().upper())
+
+print(len('Test'))
