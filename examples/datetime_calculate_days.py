6fb6fabc9d0522e23877170ded14a76e15022ee6
samin <samincgs@gmail.com>
2024-09-26 21:31:32 -0400

    working on script to see how to calculate how long it takes to do soemthing

--- examples/datetime_calculate_days.py
+++ examples/datetime_calculate_days.py
@@ -2,3 +2,8 @@ import datetime
 import calendar
 
 # write a script to calculate the number of days, weeks or months to reach specific goals
+
+# pay off a credit card
+balance = 5000
+interest_rate = 13 * 0.01
+monthly_payment = 500
