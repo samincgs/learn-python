341a5dbdcd115f57c4d2f8c8f1793670fa0ce52e
samin <samincgs@gmail.com>
2024-09-28 21:04:47 -0400

    reviewing more about datetime and sqlite

--- examples/datetime_calculate_days.py
+++ examples/datetime_calculate_days.py
@@ -7,3 +7,10 @@ import calendar
 balance = 5000
 interest_rate = 13 * 0.01
 monthly_payment = 500
+
+today = datetime.date.today() # todays date
+days_in_current_month = calendar.monthrange(today.year, today.month)[1] # first day of the next month
+
+days_till_end_month = days_in_current_month - today.day
+
+start_date = today + datetime.timedelta(days=days_till_end_month + 1)
--- modules/sqllite/sqllite_demo.py
+++ modules/sqllite/sqllite_demo.py
@@ -1,43 +1,7 @@
 import sqlite3
 from employee import Employee
 
-def insert_emp(emp):
-    with conn:
-        c.execute("INSERT INTO employees VALUES (:first, :last, :pay)", {'first' : emp.first, 'last': emp.last, 'pay': emp.pay})
-        
-def get_emp_by_name(lastname):
-    c.execute("SELECT * FROM employees WHERE last=:last", {'last': lastname})
-    return c.fetchall()
-
-conn = sqlite3.connect('employee.db')
-# conn = sqlite3.connect(':memory:') runs in memory and starts completely fresh each time
-
-c = conn.cursor()
-
-emp_1 = Employee('John', 'Doe', '50000')
-emp_2 = Employee('Jane', 'Mills', '35000')
-emp_3 = Employee('Josh', 'Cane', '75000')
-
-
-# c.execute('''
-#           CREATE TABLE employees (
-#               first text,
-#               last text,
-#               pay integer
-#           )
-#           ''')
-
-# c.execute("INSERT INTO employees VALUES (?, ?, ?)", (emp_1.first, emp_1.last, emp_1.pay))
-
-# c.execute("INSERT INTO employees VALUES (:first, :last, :pay)", {'first' : emp_2.first, 'last': emp_2.last, 'pay': emp_2.pay})
-
-# insert_emp(emp_3)
-print(get_emp_by_name('Cane'))
-
-# c.execute("SELECT * FROM employees WHERE last=:last", {'last': 'Smith'})
-
-# print(c.fetchall())
-
-# conn.commit()
-
-conn.close()
+# Sqlite is used for database functionality 
+# use to spin up a database but dont want a full on mySQL or PostgresSQL database
+# it is lightweight and medium sized and database lives on a disk
+# use for testing and prototyping before porting it to a bigger database if needed
