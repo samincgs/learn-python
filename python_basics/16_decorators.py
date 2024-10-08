3217fd918a16822adfbbaf3fa23126003c6aabae
samin <samincgs@gmail.com>
2024-10-08 19:56:14 -0400

    started learning more about decorators

--- python_basics/16_decorators.py
+++ python_basics/16_decorators.py
@@ -1,1 +1,11 @@
 # learning more about decorators
+
+def outer_function():
+    message = 'Hi'
+    
+    def inner_function():
+        print(message)
+    return inner_function()
+
+
+outer_function()
